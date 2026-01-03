6a496144a838c3c31333169e68fafe32ce1b1b57
Andreas Würl <andi@tryb.de>
2026-01-03 11:28:33 +0100

    add gh action

---
+++ src/strip/Strip.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Andreas W. on 02.01.26.
+//
+
+#include "Strip.h"
+
+namespace Strip {
+    void Strip::setPixelColor(PixelIndex pixel_index, Color color) {
+        // Default implementation - can be overridden by subclasses
+    }
+
+    Color Strip::getPixelColor(PixelIndex pixel_index) const {
+        // Default implementation - can be overridden by subclasses
+        return 0;
+    }
+}
